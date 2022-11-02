85b4dda6a1e1655bd3b7f3869d8739271ac39712
Mamadou <mamadou.thiongane@grenoble-inp.org>
2022-11-02 04:02:58 +0100

    Import du projet

---
+++ approximate_pi.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""Calcul d'une valeur approximative de pi à l'aide d'une simulation de Monte-Carlo."""
+
+import sys
+from random import uniform
+from collections import namedtuple
+
+Point = namedtuple("Point", "x y in_circle")
+
+def simulation(n):
+    """Génère aléatoirement n points puis renvoie la valeur approximative de pi et la liste des points."""
+    count = 0
+    points = []
+    for _ in range(n):
+        x = uniform(-1, 1)
+        y = uniform(-1, 1)
+        in_circle = x**2 + y**2 < 1
+        if in_circle:
+            count += 1
+        if __name__ == "__main__":
+            continue
+        points.append(Point(x, y, in_circle))
+    return 4 * count/n, points
+
+def main():
+    """Calcule une valeur approximative de pi."""
+
+    if len(sys.argv) != 2:
+        sys.exit(f"use : {sys.argv[0]} nb_points")
+    
+    n = int(sys.argv[1])
+    res, _ = simulation(n)
+    print(res)
+
+if __name__ == "__main__":
+    main()
