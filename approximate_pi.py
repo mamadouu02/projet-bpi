2ca47df3f6476383e05d261a31c44084316da960
Mamadou <mamadou.thiongane@grenoble-inp.org>
2022-11-09 03:15:49 +0100

    Import du projet

--- approximate_pi.py
+++ approximate_pi.py
@@ -6,48 +6,41 @@ import sys
 from random import uniform
 
 class Point:
-    
+    """Définition de la classe Point."""
     def __init__(self, x, y, in_circle):
         self.x = x
         self.y = y
         self.in_circle = in_circle
 
-def simulation(n):
-    """Génère aléatoirement n points puis renvoie la valeur approximative de pi et la liste des points."""
+def simulation(n_points):
+    """Génère aléatoirement n points puis et la valeur approximative de pi."""
 
     count = 0
     values = []
     points = []
 
-    for i in range(1, n+1):
+    for i in range(1, n_points+1):
         x = uniform(-1, 1)
         y = uniform(-1, 1)
         in_circle = x**2 + y**2 < 1
         if in_circle:
             count += 1
-        
         if __name__ == "__main__":
             continue
-        
-        if i % (n // 10) == 0:
+        if i % (n_points // 10) == 0:
             values.append(4 * count/i)
         points.append(Point(x, y, in_circle))
 
     if __name__ == "__main__":
-        return 4 * count/n
+        return 4 * count/n_points
 
     return values, points
 
 def main():
     """Calcule une valeur approximative de pi."""
-
-    if len(sys.argv) != 2:
-        sys.exit(f"usage: {sys.argv[0]} n_points")
-    
-    n = int(sys.argv[1])
-    pi = simulation(n)
-    print(pi)
+    n_points = int(sys.argv[1])
+    res = simulation(n_points)
+    print(res)
 
 if __name__ == "__main__":
     main()
-    
--- draw.py
+++ draw.py
@@ -6,7 +6,7 @@ import sys
 import subprocess
 import approximate_pi
 
-def generate_ppm_file(size, points, state, pi):
+def generate_ppm_file(size, points, state, res):
     """Génère une image PPM."""
 
     colors = ["w" for _ in range(size ** 2)]
@@ -20,9 +20,9 @@ def generate_ppm_file(size, points, state, pi):
         else:
             colors[index] = 'm'
 
-    pi = str(pi).replace('.', '-')
-    filename = f'img{state}_{pi}.ppm'
-    with open(filename, 'w') as file:
+    res = str(res).replace('.', '-')
+    filename = f'img{state}_{res}.ppm'
+    with open(filename, 'w', encoding='utf-8') as file:
         file.write("P3\n")
         file.write(f"{size} {size}\n")
         file.write("1\n")
@@ -36,28 +36,27 @@ def generate_ppm_file(size, points, state, pi):
 
 def main():
     """Génère une image animée."""
-    
+
     if len(sys.argv) != 4:
         sys.exit(f"usage: {sys.argv[0]} image_size n_points n_digits")
-    
+
     size = int(sys.argv[1])
-    n = int(sys.argv[2])
+    n_points = int(sys.argv[2])
     digits = int(sys.argv[3])
 
     if size < 100:
         raise ValueError("image_size must be an integer greater than 100")
-    if n < 100:
+    if n_points < 100:
         raise ValueError("n_points must be an integer greater than 100")
     if digits < 1 or digits > 5:
         raise ValueError("n_digits must be an integer between 1 and 5")
-    
-    values, points = approximate_pi.simulation(n)
+    values, points = approximate_pi.simulation(n_points)
 
     for state in range(10):
-        pi = round(values[state], digits)
-        generate_ppm_file(size, points[:n//10 * (state + 1)], state, pi)
+        res = round(values[state], digits)
+        generate_ppm_file(size, points[:n_points//10 * (state + 1)], state, res)
 
-    subprocess.run("convert -delay 100 -loop 0 img*.ppm img.gif", shell=True)
+    subprocess.run("convert -delay 100 -loop 0 img*.ppm img.gif", shell=True, check=True)
 
 if __name__ == "__main__":
     main()
