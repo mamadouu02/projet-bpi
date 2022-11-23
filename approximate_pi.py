8cb061bc28ace222c63a48d5f74701493676a30e
Mamadou <mamadou.thiongane@grenoble-inp.org>
2022-11-23 04:51:01 +0100

    Import du projet

--- approximate_pi.py
+++ approximate_pi.py
@@ -2,41 +2,38 @@
 
 """Calcul d'une valeur approximative de pi à l'aide d'une simulation de Monte-Carlo."""
 
-import sys
+from sys import argv
 from random import uniform
 from collections import namedtuple
 
 Point = namedtuple("Point", "x, y, in_circle")
 
-def simulation(n_points):
+def generator(n_points):
     """Génère aléatoirement n points et la valeur approximative de pi."""
 
     count = 0
-    values = []
-    points = []
-
     for i in range(1, n_points+1):
         x = uniform(-1, 1)
         y = uniform(-1, 1)
         in_circle = x**2 + y**2 < 1
         if in_circle:
             count += 1
+
         if __name__ == "__main__":
             continue
+
+        yield Point(x, y, in_circle)
         if i % (n_points // 10) == 0:
-            values.append(4 * count/i)
-        points.append(Point(x, y, in_circle))
+            yield 4 * count/i
 
     if __name__ == "__main__":
-        return 4 * count/n_points
-
-    return values, points
+        yield 4 * count/n_points
 
 def main():
     """Calcule une valeur approximative de pi."""
-    n_points = int(sys.argv[1])
-    value = simulation(n_points)
-    print(value)
+    n_points = int(argv[1])
+    for value in generator(n_points):
+        print(value)
 
 if __name__ == "__main__":
     main()
--- draw.py
+++ draw.py
@@ -2,24 +2,23 @@
 
 """Génération d'une image animée représentant une simulation."""
 
-import subprocess
 import sys
-import approximate_pi
+from subprocess import run
+from approximate_pi import generator
 
 def pixel_index(x, y, imagesize):
     """Renvoie l'index d'un pixel."""
     return imagesize * (imagesize - 1 - y) + x
 
-def point_to_color(points, colors, imagesize):
-    """Convertit les points en couleurs RGB."""
-    for point in points:
-        x = int((point.x + 1) * (imagesize - 1) // 2)
-        y = int((point.y + 1) * (imagesize - 1) // 2)
-        index = pixel_index(x, y, imagesize)
-        if point.in_circle:
-            colors[index] = "0 0 1 "
-        else:
-            colors[index] = "1 0 1 "
+def point_to_color(point, colors, imagesize):
+    """Convertit un point en couleurs RGB."""
+    x = int((point.x + 1) * (imagesize - 1) // 2)
+    y = int((point.y + 1) * (imagesize - 1) // 2)
+    index = pixel_index(x, y, imagesize)
+    if point.in_circle:
+        colors[index] = "0 0 1 "
+    else:
+        colors[index] = "1 0 1 "
 
 def digits_to_index(digitsindex, digit, width, height, linewidth, xmin, ymin, imagesize):
     """Ajoute la liste des index des pixels utilisés pour l'affichage des chiffres."""
@@ -112,7 +111,6 @@ def main():
     if n_digits < 1 or n_digits > 5:
         raise ValueError("n_digits must be an integer between 1 and 5")
 
-    step = n_points//10
     spacing = image_size * 1//100 + 1
     digit_width = ((image_size * 1//3) + 20) // (n_digits + 1)
     line_width = image_size * 4//1000 + 1
@@ -120,36 +118,39 @@ def main():
     display_width = (n_digits + 1) * (spacing + digit_width) + point_size
     display_height = image_size * 15//100 + 5
     y_min = (image_size - display_height) // 2
-    
-    values, points = approximate_pi.simulation(n_points)
+
+    state = 0
     colors = ["0 0 0 " for _ in range(image_size ** 2)]
     memory = []
 
-    for state in range(10):
-        value = values[state]
-        digits = f"{value:.{n_digits}f}"
-        filename = f"img{state}_{digits.replace('.', '-')}.ppm"
+    for elem in generator(n_points):
+        if not isinstance(elem, float):
+            point = elem
+            point_to_color(point, colors, image_size)
+        else:
+            value = elem
 
-        memory_to_color(memory, colors)
-        memory = []
+            digits = f"{value:.{n_digits}f}"
+            digits_index = []
+            x_min = (image_size - display_width) // 2
+            for digit in digits:
+                width = digit_width
+                if digit == ".":
+                    width = point_size
+                digits_to_index(digits_index, digit, width, display_height, line_width,
+                x_min, y_min, image_size)
+                x_min += width + spacing
+            digit_to_color(memory, colors, digits_index)
 
-        point_to_color(points[step * state:step * (state + 1)], colors, image_size)
+            filename = f"img{state}_{digits.replace('.', '-')}.ppm"
+            generate_ppm_file(image_size, colors, filename)
 
-        x_min = (image_size - display_width) // 2
-        digits_index = []
-        for digit in digits:
-            width = digit_width
-            if digit == ".":
-                width = point_size
-            digits_to_index(digits_index, digit, width, display_height, line_width,
-            x_min, y_min, image_size)
-            x_min += width + spacing
+            memory_to_color(memory, colors)
+            memory = []
 
-        digit_to_color(memory, colors, digits_index)
+            state += 1
 
-        generate_ppm_file(image_size, colors, filename)
-
-    subprocess.run("convert -delay 100 -loop 0 img*.ppm img.gif", shell=True, check=False)
+    run("convert -delay 100 -loop 0 img*.ppm img.gif", shell=True, check=False)
 
 if __name__ == "__main__":
     main()
